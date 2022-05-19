f2c6729037c0afe2035dc5332291975c7595d36d
Lara Flanderka <lflanderka@gmail.com>
2022-05-19 16:47:22 +0200

    Challenge 02; bookmarks controller all but destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'lists', to: 'lists#index'
+  resources :lists do
+    resources :bookmarks, only: [ :new, :create ]
+  end
 
-  get 'lists/new', to: 'lists#new'
-  post 'lists', to: 'lists#create'
-
-  get 'lists/:id', to: 'lists#show', as: 'list'
-
-  get 'lists/:id/bookmarks/new', to: 'bookmarks#new'
+  delete 'bookmarks/:id', to: 'bookmarks#destroy'
 end
