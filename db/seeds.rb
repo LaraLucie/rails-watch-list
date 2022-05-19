b76e9010ffa240e4c39913c7b0c1d99507eef65f
Lara Flanderka <lflanderka@gmail.com>
2022-05-19 12:38:29 +0200

    Added movies in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Start'
+
+5.times do
+  Movie.create({
+    title: Faker::JapaneseMedia::StudioGhibli.movie,
+    overview: Faker::JapaneseMedia::StudioGhibli.quote,
+    poster_url: "https://#{Faker::JapaneseMedia::StudioGhibli.character}.com",
+    rating: rand(0...5)
+  })
+end
+puts 'End'
